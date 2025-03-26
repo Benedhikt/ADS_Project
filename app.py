3bd4cfc27791ce4c7c64321281550b0d21690118
Benedict <84344015+Benedhikt@users.noreply.github.com>
2025-03-26 00:00:33 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.model_selection import KFold, cross_val_score
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVR
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.linear_model import Lasso
+from sklearn.linear_model import ElasticNet
+import joblib
+
+
+# Set the title of the app
+st.title("🏡 Boston Housing Price Prediction")
+st.write("This app predicts house prices based on various features.")
